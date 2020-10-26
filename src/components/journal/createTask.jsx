3a88e958fe8eefa594e3c823dc22e84a237ffda9
Fengrui Gan <fengruigan@gmail.com>
2020-10-25 23:17:29 -0400

    add new createTask

--- src/components/journal/createTask.jsx
+++ src/components/journal/createTask.jsx
@@ -1,34 +1,69 @@
-import React, { Component } from "react";
-import "../../css/journal/createTask.css";
+import React, { Fragment } from "react";
+import { SettingOutlined, SmileFilled } from "@ant-design/icons";
+import { Input, Popover, Card, Tooltip } from "antd";
 
-class CreateTask extends Component {
-	state = {};
-	render() {
-		return (
-			<div>
-				<form>
-					<div className="row">
-						<button
-							type="button"
-							className="new-task btn-floating btn-small waves-effect waves-light blue"
-						>
-							<i className="material-icons">add</i>
-						</button>
-						{/* <span> </span>
-                    <button
-					type="button"
-					className="new-task btn-floating btn-small waves-effect waves-light red"
-                    >
-					<i class="material-icons">clear</i>
-				</button> */}
-						<div className="input-field inline col s9">
-							<input type="text" placeholder=" new task" />
-						</div>
-					</div>
-				</form>
-			</div>
-		);
-	}
-}
+const types = [
+	{ content: "Todo", icon: <SmileFilled /> },
+	{ content: "Thought", icon: <SettingOutlined /> },
+	{ content: "Note", icon: <SmileFilled /> },
+	{ content: "Miscellaneous", icon: <SettingOutlined /> },
+	{ content: "Misc.", icon: <SettingOutlined /> },
+];
+
+const gridStyle = {
+	width: "25%",
+	textAlign: "center",
+	padding: "6px",
+};
+
+const popContent = (
+	<Card>
+		{types.map((item) => {
+			return (
+				<Card.Grid style={gridStyle}>
+					<Tooltip
+						title={item.content}
+						placement={"top"}
+						arrowPointAtCenter={false}
+					>
+						<p style={{ fontSize: "1.3em", marginBottom: 0 }}>
+							{item.icon}
+						</p>
+					</Tooltip>
+					<p
+						style={{
+							color: "gray",
+							marginBottom: 0,
+							maxWidth: "4em",
+							overflow: "hidden",
+							textOverflow: "ellipsis",
+						}}
+					>
+						{item.content}
+					</p>
+				</Card.Grid>
+			);
+		})}
+	</Card>
+);
+
+const listType = (
+	<Popover content={popContent} trigger={"click"} placement={"bottom"}>
+		<SettingOutlined />
+	</Popover>
+);
+
+const CreateTask = () => {
+	return (
+		<Fragment>
+			{" "}
+			<Input
+				addonBefore={listType}
+				bordered={true}
+				placeholder="Todo content"
+			/>
+		</Fragment>
+	);
+};
 
 export default CreateTask;
--- src/components/journal/journal.jsx
+++ src/components/journal/journal.jsx
@@ -1,44 +1,52 @@
 import React, { useState, useEffect } from "react";
-// import CreateTask from "./createTask";
-// import Todo from "./todo";
-// import getDate from "../../util/getDate";
+import CreateTask from "./createTask";
+import { LoadingOutlined, SettingOutlined } from "@ant-design/icons";
+import { Empty, List } from "antd";
 import "../../css/journal/journal.css";
-import { LoadingOutlined } from "@ant-design/icons";
 
-// class Journal extends Component {
 const Journal = ({ currentDate }) => {
 	let [todos, setTodos] = useState({ data: null, loading: true });
 
 	useEffect(() => {
 		const fetchData = async () => {
-			let todoNum = Math.floor(Math.random() * 10 + 1);
-			let temp = [];
-			for (let i = 0; i < todoNum; i++) {
-				let id = Math.floor(Math.random() * 200);
-				const response = await fetch(
-					"https://jsonplaceholder.typicode.com/todos/" + id
-				);
-				const json = await response.json();
-				temp.push(json.title);
+			let date = currentDate.format("yyyy-MM-DD");
+			const response = await fetch("http://localhost:8000/api/" + date);
+			let json;
+			try {
+				json = await response.json();
+			} catch {
+				json = [];
 			}
-			setTodos({ data: temp, loading: false });
+			setTodos({ data: json, loading: false });
 		};
 		fetchData();
-	}, []);
+	}, [currentDate]);
 
-	// renderDate = (match) => {
-	// 	if (match !== undefined && match.params.date !== undefined) {
-	// 		return getDate(match.params.date);
-	// 	} else {
-	// 		return getDate();
-	// 	}
-	// };
+	const generateList = () => {
+		if (todos.data === null || todos.data.length === 0) {
+			return <Empty />;
+		} else {
+			return (
+				<List
+					dataSource={todos.data}
+					renderItem={(item) => (
+						<List.Item>
+							<List.Item.Meta
+								avatar={<SettingOutlined />}
+								description={item.content}
+							/>
+						</List.Item>
+					)}
+				></List>
+			);
+		}
+	};
+
 	return (
 		<div className="journal">
 			<h1 className="date">{currentDate.format("ddd MMM. DD, yyyy")}</h1>
 			<hr className="wide-divider" />
 			<div className="todo-list">
-				<h5>Todos Today</h5>
 				{todos.loading ? (
 					<LoadingOutlined
 						style={{
@@ -50,25 +58,14 @@ const Journal = ({ currentDate }) => {
 						}}
 					/>
 				) : (
-					<ul>
-						{todos.data.map((content, idx) => {
-							return (
-								<li key={idx} style={{ listStyle: "initial" }}>
-									{content}
-								</li>
-							);
-						})}
-					</ul>
+					generateList()
 				)}
-				{/* <Todo />
-					<Todo />
-					<Todo />
-					<CreateTask /> */}
 			</div>
-			<div className="divider"></div>
+			<CreateTask />
+			{/* <div className="divider"></div>
 			<div className="general-list">
 				<h5>Note</h5>
-			</div>
+			</div> */}
 		</div>
 	);
 };
