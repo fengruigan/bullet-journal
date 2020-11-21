00e86b58c442195b5525be72399a6d59d9decc89
Fengrui Gan <fengruigan@gmail.com>
2020-11-21 12:17:32 -0500

    :lipstick:: style journal page and input field

--- src/components/journal/inputField.jsx
+++ src/components/journal/inputField.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from "react";
-import { PlusOutlined } from "@ant-design/icons";
-import { Input, Popover, Card, Tooltip, Form, Button, Row, Col } from "antd";
+import { SettingOutlined } from "@ant-design/icons";
+import { Input, Popover, Card, Tooltip, Form, Row, Col, Button } from "antd";
 import Emoji from "../emoji";
 import CategoryModal from "./categoryModal";
 import "../../css/journal/inputField.css";
@@ -31,50 +31,57 @@ const InputField = ({ onSubmit }) => {
 	// content to go into the popover
 	const popContent = (
 		<Card id="popover">
-			{/* This will be the new category button */}
+			{/* This will be the manage category button */}
 			<Card.Grid
 				className="category-grid"
 				onClick={() => {
 					setModalVisible(true);
 				}}
 			>
-				<Tooltip title={"Add new tag"} placement={"top"}>
+				<Tooltip title={"Manage categories"} placement={"top"}>
 					<div className="category-new">
-						<PlusOutlined />
+						<SettingOutlined />
 					</div>
 				</Tooltip>
 			</Card.Grid>
 			{/* The list of LIST categories */}
 			{categories.map((item, index) => {
 				return (
-					<Card.Grid
-						className="category-grid"
-						key={index}
-						onClick={() => {
-							setCategoryIndex(index);
-						}}
+					<Tooltip
+						title={item.category}
+						placement="top"
+						arrowPointAtCenter
 					>
-						<Tooltip
-							title={item.category}
-							placement="top"
-							arrowPointAtCenter={true}
+						<Card.Grid
+							className="category-grid"
+							key={index}
+							onClick={() => {
+								setCategoryIndex(index);
+							}}
 						>
 							<div style={{ fontSize: "1.3em", marginBottom: 0 }}>
 								<span style={{ padding: "0" }}>
 									<Emoji symbol={item.emoji} />
 								</span>
 							</div>
-						</Tooltip>
-						<p className="category-description">{item.category}</p>
-					</Card.Grid>
+							<p className="category-description">
+								{item.category}
+							</p>
+						</Card.Grid>
+					</Tooltip>
 				);
 			})}
 		</Card>
 	);
 
 	const listCategory = (
-		<Popover content={popContent} trigger={"click"} placement={"bottom"}>
-			<span style={{ padding: "0", fontSize: "1.5em" }}>
+		<Popover
+			content={popContent}
+			trigger={"click"}
+			placement={"bottomLeft"}
+			arrowPointAtCenter
+		>
+			<span style={{ fontSize: "1.5em" }}>
 				<Emoji symbol={categories[categoryIndex].emoji} />
 			</span>
 		</Popover>
@@ -92,43 +99,50 @@ const InputField = ({ onSubmit }) => {
 					onReset();
 				}}
 			>
-				<Input.Group>
-					<Row>
-						<Col>
-							<Form.Item
-								name="content"
-								rules={[
-									{
-										required: "true",
-										message:
-											"Please write down your notes here",
-									},
-								]}
+				<Row>
+					<Col xs={19} sm={21}>
+						<Form.Item
+							name="content"
+							rules={[
+								{
+									required: "true",
+									message:
+										"Please write down your notes here",
+								},
+							]}
+						>
+							<Input
+								style={{
+									paddingLeft: "0.7em",
+								}}
+								size={"large"}
+								allowClear={true}
+								addonBefore={listCategory}
+								placeholder="Jot down your note here"
+								// addonAfter={
+								// 	<span
+								// 		onClick={() => {
+								// 			form.submit();
+								// 		}}
+								// 	>
+								// 		Add
+								// 	</span>
+								// }
+							/>
+						</Form.Item>
+					</Col>
+					<Col>
+						<Form.Item>
+							<Button
+								type="primary"
+								htmlType="submit"
+								size="large"
 							>
-								<Input
-									style={{
-										paddingLeft: "0.7em",
-									}}
-									size={"large"}
-									allowClear={true}
-									addonBefore={listCategory}
-									placeholder="Jot down your note here"
-								/>
-							</Form.Item>
-						</Col>
-						<Col>
-							<Form.Item>
-								<Button
-									type="primary"
-									htmlType="submit"
-									size="large"
-								>
-									Add
-								</Button>
-							</Form.Item>
-						</Col>
-					</Row>
-				</Input.Group>
+								Add
+							</Button>
+						</Form.Item>
+					</Col>
+				</Row>
 			</Form>
 
 			{/* Modal for customizing list category */}
